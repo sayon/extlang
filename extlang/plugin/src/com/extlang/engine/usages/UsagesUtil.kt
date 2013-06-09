57e4a42b802265dbe66a562786d0533135f1365f
igorjirkov <igorjirkov@gmail.com>
2013-06-09 19:19:29 +0000

    Go to declaration works with a minor bug

--- extlang/plugin/src/com/extlang/engine/usages/UsagesUtil.kt
+++ extlang/plugin/src/com/extlang/engine/usages/UsagesUtil.kt
@@ -8,21 +8,29 @@ import com.extlang.engine.model.TokIdentifier
 import com.intellij.openapi.util.TextRange
 import java.util.ArrayList
 import com.extlang.util.Util
+import com.intellij.psi.PsiNamedElement
+import com.intellij.psi.PsiReferenceBase.Immediate
+import com.intellij.openapi.project.Project
 
 
 class UsagesUtil
 {
     class object {
-        public fun getReferencesByElement(element: PsiElement, context: ProcessingContext): Array<PsiReference> {
-            if (element is ELASTNode)
-            {
-                val key = element.getName()
-                val identifiers = Util.findIdentifiers(element.getProject(), key)
-                val ret = identifiers.map {(node) -> ELReference(node.getPsi(), node.getTextRange()) }.toArray(array() : Array<PsiReference>)
-                return ret
-            }
-            else
-                return array()
+
+
+        public fun firstIdentifier(element: PsiNamedElement): PsiElement ?
+        {
+            val name = element.getName()
+            val project = element.getProject()
+            val identifiers = Util.findIdentifiers(project, name)
+            return if (identifiers.size > 0) identifiers[0].getPsi() else null
         }
+            public fun firstIdentifierReference(element: PsiNamedElement): PsiReference
+        {
+            val ident = firstIdentifier(element)
+            val ref = ELReference(element, element.getTextRange(), ident!!)
+            return ref
+        }
+
     }
 }
