cdfa0c591f997d8164200e7c120e9a341e084fbf
aristarkhov.m@gmail.com <aristarkhov.m@gmail.com@30eb7cfa-a84c-23e2-01e5-c64fe929a467>
2013-06-11 09:36:01 +0000

    trying to update

--- trunk/extlang-test-inheritance/e1/extlang/plugin/src/com/extlang/engine/ELLanguage.kt
+++
@@ -1,9 +0,0 @@
-package com.extlang.engine
-
-import com.intellij.lang.Language
-
-public open class ELLanguage(): Language("EL") {
-    class object {
-        public val INSTANCE: ELLanguage? = ELLanguage()
-    }
-}
